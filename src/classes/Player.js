78797728b5428fe391eab19c9e784747a60a2c64
lelonS <35070147+lelonS@users.noreply.github.com>
2023-08-22 13:42:40 +0200

    Add method for user to input name
    Co-Authored-By: diana2025 <113677361+diana2025@users.noreply.github.com>
    Co-Authored-By: babblin <35327157+nibla007@users.noreply.github.com>

---
+++ src/classes/Player.js
@@ -0,0 +1,12 @@
+class Player {
+  constructor(name) {
+    this.name = name;
+  }
+
+  setNameFromInput(plrNumber) {
+    const name = prompt('Enter name for Player ' + plrNumber + ':');
+    this.name = name;
+  }
+}
+
+
