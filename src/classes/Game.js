4fbdd86f38207ac04933e7ea8fc4d12dfca067bb
lelonS <35070147+lelonS@users.noreply.github.com>
2023-08-22 15:16:42 +0200

    Use console for input instead of prompt
    Use console for input instead of prompt to use the same input method as making moves
    
    Co-Authored-By: diana2025 <113677361+diana2025@users.noreply.github.com>

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -3,15 +3,32 @@ class Game {
     this.playerCount = 2;
     this.createPlayers();
 
-    console.log('Players:', this.players);
+    this.expectedPlrName = 0;
+
+    // console.log('Players:', this.players);
   }
 
   createPlayers() {
     this.players = [];
     for (let i = 0; i < this.playerCount; i++) {
-      const player = new Player();
-      player.setNameFromInput(i + 1);
+      const player = new Player('Player ' + (i + 1));
       this.players.push(player);
+    }
+
+    console.log('Use "game.inputName(name)" to set player names.');
+    console.log('Player 1:');
+  }
+
+  inputName(name) {
+    if (this.expectedPlrName < this.playerCount) {
+      this.players[this.expectedPlrName].name = name
+      this.expectedPlrName++;
+      if (this.expectedPlrName < this.playerCount) {
+        console.log('Player ' + (this.expectedPlrName + 1) + ':');
+      } else {
+        console.log('All players named.');
+        console.log('Players:', this.players);
+      }
     }
   }
 }
--- src/classes/Player.js
+++ src/classes/Player.js
@@ -2,9 +2,4 @@ class Player {
   constructor(name) {
     this.name = name;
   }
-
-  setNameFromInput(plrNumber) {
-    const name = prompt('Enter name for Player ' + plrNumber + ':');
-    this.name = name ? name : 'Player ' + plrNumber;
-  }
 }
