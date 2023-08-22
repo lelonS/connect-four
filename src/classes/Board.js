5bcd17c832b05672391c352b79cc682f206b9edd
lelonS <35070147+lelonS@users.noreply.github.com>
2023-08-22 15:05:11 +0200

    Add Board class
    Co-Authored-By: diana2025 <113677361+diana2025@users.noreply.github.com>

---
+++ src/classes/Board.js
@@ -0,0 +1,47 @@
+
+const GameStates = Object.freeze({
+  Playing: 'playing',
+  Win: 'win',
+  Draw: 'draw'
+});
+
+class Board {
+  constructor() {
+    this.playerCount = 2;
+    this.colCount = 7;
+    this.rowCount = 6;
+
+    this.gameState = GameStates.Playing;
+    this.turn = 0;
+
+    this.createEmptyBoard();
+  }
+
+  createEmptyBoard() {
+    this.board = [];
+    for (let i = 0; i < this.colCount; i++) {
+      this.board.push([]);
+    }
+  }
+
+  nextTurn() {
+    this.turn = (this.turn + 1) % this.playerCount;
+  }
+
+  makeMove(col) {
+    this.board[col].push(this.turn);
+    this.nextTurn();
+  }
+
+  render() {
+    let output = '';
+    for (let row = this.rowCount - 1; row >= 0; row--) {
+      for (let col = 0; col < this.colCount; col++) {
+        const cell = this.board[col][row];
+        output += cell === undefined ? '_' : cell;
+      }
+      output += '\n';
+    }
+    console.log(output);
+  }
+}
