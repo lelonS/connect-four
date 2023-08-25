eae58d487d88925ea846efd74c72ecf15dbdfd87
Sherif <sherifmagdy@gmail.com>
2023-08-25 08:47:46 +0200

    added isFull and draw printing

--- src/classes/Board.js
+++ src/classes/Board.js
@@ -1,4 +1,3 @@
-
 const GameStates = Object.freeze({
   Playing: 'playing',
   Win: 'win',
@@ -10,42 +9,56 @@ class Board {
   get colCount() { return 7; }
   get rowCount() { return 6; }
 
-  constructor() {
-    this.gameState = GameStates.Playing;
-    this.turn = 0;
+  get isFull() {
+      for (let i = 0; i < this.colCount; i++) {
+          if (this.board[i].length !== this.rowCount) {
+              return false;
+          }
+      }
+      return true;
+  }
 
-    this.createEmptyBoard();
+  constructor() {
+      this.gameState = GameStates.Playing;
+      this.turn = 0;
+
+      this.createEmptyBoard();
   }
 
   createEmptyBoard() {
-    this.board = [];
-    for (let i = 0; i < this.colCount; i++) {
-      this.board.push([]);
-    }
+      this.board = [];
+      for (let i = 0; i < this.colCount; i++) {
+          this.board.push([]);
+      }
   }
 
   nextTurn() {
-    this.turn = (this.turn + 1) % this.playerCount;
+      this.turn = (this.turn + 1) % this.playerCount;
   }
 
   makeMove(col) {
-    if (col < 0 || col >= this.colCount) {
-      return false;
-    }
-    this.board[col].push(this.turn);
-    this.nextTurn();
-    return true;
+      if (col < 0 || col >= this.colCount) {
+          return false;
+      }
+      this.board[col].push(this.turn);
+      if (this.isFull) {
+          this.gameState = GameStates.Draw;
+          console.log('game is drawed!');
+      } else {
+          this.nextTurn();
+      }
+      return true;
   }
 
   writeToConsole() {
-    let output = '';
-    for (let row = this.rowCount - 1; row >= 0; row--) {
-      for (let col = 0; col < this.colCount; col++) {
-        const cell = this.board[col][row];
-        output += cell === undefined ? '_' : cell;
+      let output = '';
+      for (let row = this.rowCount - 1; row >= 0; row--) {
+          for (let col = 0; col < this.colCount; col++) {
+              const cell = this.board[col][row];
+              output += cell === undefined ? '_' : cell;
+          }
+          output += '\n';
       }
-      output += '\n';
-    }
-    console.log(output);
+      console.log(output);
   }
 }
