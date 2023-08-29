3161c3b231280eef0d71cb22e00d41f6734869c2
Sherif <sherifmagdy@gmail.com>
2023-08-29 11:24:06 +0200

    FEAT: added tests for players

--- tests/Player.test.js
+++ tests/Player.test.js
@@ -1,5 +1,17 @@
 require('./load-all-classes.js');
 
-test('Temp', () => {
-  expect(true).toBe(true);
+test('Player creation', () => {
+  const player = new Player('test', 'blue');
+  expect(player.name).toBe('test');
+})
+
+test('isValidName test', () => {
+  expect(Player.isValidName('123test')).toBe(false);
+  expect(Player.isValidName('@@@#######test')).toBe(false);
+  expect(Player.isValidName('test')).toBe(true);
+})
+
+test('player toString', () => {
+  const player = new Player('test', 'blue');
+  expect(player.toString()).toBe('test (blue)');
 })
