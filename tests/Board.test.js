838ca1df4ac47ee500ca00815e90b79df0465a25
lelonS <35070147+lelonS@users.noreply.github.com>
2023-08-29 10:30:58 +0200

    Create test for getCell method

--- tests/Board.test.js
+++ tests/Board.test.js
@@ -11,6 +11,7 @@ const fullBoardDraw = [
   [0, 1, 0, 1, 0, 1]
 ];
 
+// Test initial board state
 test('Initial board variables are correct', () => {
   const board = new Board();
   // Check Connect-4 constants
@@ -26,9 +27,35 @@ test('Initial board variables are correct', () => {
   expect(board.board).toEqual(emptyBoard);
 });
 
+// Test createEmptyBoard()
 test('createEmptyBoard() creates an empty board', () => {
   const board = new Board();
   board.board = fullBoardDraw;
   board.createEmptyBoard();
   expect(board.board).toEqual(emptyBoard);
+});
+
+// Test getCell()
+test('getCell() returns null for out of bounds', () => {
+  const board = new Board();
+  // Negative values
+  expect(board.getCell(-1, 0)).toBe(null); // col
+  expect(board.getCell(0, -1)).toBe(null); // row
+
+  // Too large values
+  expect(board.getCell(7, 0)).toBe(null); // col
+  expect(board.getCell(0, 6)).toBe(null); // row
+});
+
+test('getCell() returns null for empty cells', () => {
+  const board = new Board();
+  // Empty board
+  expect(board.getCell(0, 0)).toBe(null);
+});
+
+test('getCell() returns player index for non-empty cells', () => {
+  const board = new Board();
+  board.board = fullBoardDraw;
+  expect(board.getCell(0, 0)).toBe(0);
+  expect(board.getCell(6, 5)).toBe(1);
 });
