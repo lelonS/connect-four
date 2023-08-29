f5e0aa3a6d1d3a3d90590ee7d3635482a1179595
Sherif <sherifmagdy@gmail.com>
2023-08-29 11:31:47 +0200

    added test

--- tests/Player.test.js
+++ tests/Player.test.js
@@ -3,6 +3,7 @@ require('./load-all-classes.js');
 test('Player creation', () => {
   const player = new Player('test', 'blue');
   expect(player.name).toBe('test');
+  expect(player.color).toBe('blue');
 })
 
 test('isValidName test', () => {
@@ -14,4 +15,10 @@ test('isValidName test', () => {
 test('player toString', () => {
   const player = new Player('test', 'blue');
   expect(player.toString()).toBe('test (blue)');
+})
+
+test('setName', () => {
+  const player = new Player('test', 'blue');
+  player.name = 'someOtherName';
+  expect(player.name).toBe('someOtherName');
 })
