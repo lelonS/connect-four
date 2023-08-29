7d66e9707bea798339e82260330610aad1094888
babblin <nibla007@hotmail.com>
2023-08-29 14:04:32 +0200

    Add unit test for reset method

--- tests/Game.test.js
+++ tests/Game.test.js
@@ -11,3 +11,20 @@ test('New game has correct initial variables', () => {
     expect(player).toBeInstanceOf(Player);
   }
 });
+
+test('Reset method starts a new game', () => {
+  const game = new Game();
+  // Setting incorrect variables
+  game.expectedInput = 1;
+  game.players = [];
+  game.board = null;
+  game.reset();
+  // Checking if game has reset with correct initial variables
+  expect(game.board).toBeInstanceOf(Board);
+  expect(game.expectedInput).toBe(0);
+  expect(game.playerCount).toBe(2);
+  expect(game.players.length).toBe(2);
+  for (const player of game.players) {
+    expect(player).toBeInstanceOf(Player);
+  }
+});
