797d04bcb8ffad628b47b5e149840c0686d1aee8
lelonS <35070147+lelonS@users.noreply.github.com>
2023-08-29 15:23:50 +0200

    Create win positions

--- tests/BoardPositions.js
+++ tests/BoardPositions.js
@@ -33,6 +33,128 @@ class BoardPositions {
     };
   }
 
+  static get winVerticalLeftSide() {
+    return {
+      name: 'winVerticalLeftSide',
+      winnerCoords: [[0, 0], [0, 1], [0, 2], [0, 3]],
+      board: [
+        [0, 0, 0, 0],
+        [1, 1, 1],
+        [],
+        [],
+        [],
+        [],
+        []
+      ]
+    }
+  }
+
+  static get winHorizontalLeftSide() {
+    return {
+      name: 'winHorizontalLeftSide',
+      winnerCoords: [[0, 0], [1, 0], [2, 0], [3, 0]],
+      board: [
+        [0, 1],
+        [0, 1],
+        [0, 1],
+        [0],
+        [],
+        [],
+        []
+      ]
+    }
+  }
+
+  static get winDiagonalLeftSide() {
+    return {
+      name: 'winDiagonalLeftSide',
+      winnerCoords: [[0, 0], [1, 1], [2, 2], [3, 3]],
+      board: [
+        [0, 1],
+        [1, 0],
+        [1, 1, 0],
+        [0, 1, 0, 0],
+        [],
+        [],
+        []
+      ]
+    }
+  }
+
+  static get winVerticalRightSide() {
+    return {
+      name: 'winVerticalRightSide',
+      winnerCoords: [[6, 0], [6, 1], [6, 2], [6, 3]],
+      board: [
+        [],
+        [],
+        [],
+        [],
+        [0],
+        [0, 0, 0],
+        [1, 1, 1, 1]
+      ]
+    }
+  }
+
+  static get winHorizontalRightSide() {
+    return {
+      name: 'winHorizontalRightSide',
+      winnerCoords: [[3, 0], [4, 0], [5, 0], [6, 0]],
+      board: [[0],
+      [],
+      [],
+      [1],
+      [1, 0],
+      [1, 0],
+      [1, 0]
+      ]
+    }
+  }
+
+  static get winDiagonalRightSide() {
+    return {
+      name: 'winDiagonalRightSide',
+      winnerCoords: [[6, 0], [5, 1], [4, 2], [3, 3]],
+      board: [
+        [],
+        [],
+        [],
+        [0, 1, 0, 1],
+        [0, 0, 1],
+        [0, 1],
+        [1]
+      ]
+    }
+  }
+
+  static get winHorizontalTop() {
+    return {
+      name: 'winHorizontalTop',
+      winnerCoords: [[2, 5], [3, 5], [4, 5], [5, 5]],
+      board: [
+        [],
+        [],
+        [0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 1],
+        [0, 0, 1, 0, 0, 1],
+        [0, 1, 0, 1, 0, 1],
+        []
+      ]
+    }
+  }
+
+  static get winPositions() {
+    return [
+      BoardPositions.winVerticalLeftSide,
+      BoardPositions.winHorizontalLeftSide,
+      BoardPositions.winDiagonalLeftSide,
+      BoardPositions.winVerticalRightSide,
+      BoardPositions.winHorizontalRightSide,
+      BoardPositions.winDiagonalRightSide,
+      BoardPositions.winHorizontalTop
+    ];
+  }
 }
 
 module.exports = BoardPositions;
