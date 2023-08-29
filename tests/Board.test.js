d7df38d4c7a2ba6209733d8c1ae57202d145f1ac
lelonS <35070147+lelonS@users.noreply.github.com>
2023-08-29 10:22:57 +0200

    Create test for initial board variables

--- tests/Board.test.js
+++ tests/Board.test.js
@@ -1,5 +1,21 @@
 require('./load-all-classes.js');
 
-test('Temp', () => {
-  expect(true).toBe(true);
-})
+const emptyBoard = [[], [], [], [], [], [], []];
+
+
+test('Initial board variables are correct', () => {
+  const board = new Board();
+  // Check Connect-4 constants
+  expect(board.playerCount).toBe(2);
+  expect(board.colCount).toBe(7);
+  expect(board.rowCount).toBe(6);
+  expect(board.winCount).toBe(4);
+  // Check initial state
+  expect(board.gameState).toBe(Board.GameStates.Playing);
+  expect(board.turn).toBe(0);
+  expect(board.winner).toBe(null);
+  // Check board array
+  expect(board.board).toEqual(emptyBoard);
+});
+
+
