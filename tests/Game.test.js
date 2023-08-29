4a523f7d3df201094d8382b5edbdf91a9a5f5a90
babblin <nibla007@hotmail.com>
2023-08-29 13:42:50 +0200

    Add unit test for new game variables

--- tests/Game.test.js
+++ tests/Game.test.js
@@ -1,6 +1,13 @@
 require('./load-all-classes.js');
 const { getConsoleLogOutput, resetConsoleLogOutput } = require('./capture-console-log.js');
 
-test('Temp', () => {
-  expect(true).toBe(true);
-})
+test('New game has correct initial variables', () => {
+  const game = new Game();
+  expect(game.board).toBeInstanceOf(Board);
+  expect(game.expectedInput).toBe(0);
+  expect(game.playerCount).toBe(2);
+  expect(game.players.length).toBe(2);
+  for (const player of game.players) {
+    expect(player).toBeInstanceOf(Player);
+  }
+});
