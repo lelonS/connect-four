285c2d39a4a7391bdbcae7caae0a8f1b6456ac3f
lelonS <35070147+lelonS@users.noreply.github.com>
2023-08-30 17:37:54 +0200

    Add semicolons

--- tests/Player.test.js
+++ tests/Player.test.js
@@ -4,7 +4,7 @@ test('Player creation', () => {
   const player = new Player('test', 'blue');
   expect(player.name).toBe('test');
   expect(player.color).toBe('blue');
-})
+});
 
 test('isValidName test', () => {
   expect(Player.isValidName('123test')).toBe(false);
@@ -14,23 +14,23 @@ test('isValidName test', () => {
   expect(Player.isValidName('  testtt')).toBe(false);
   expect(Player.isValidName(true)).toBe(false);
   expect(Player.isValidName(123)).toBe(false);
-})
+});
 
 test('player toString', () => {
   const player = new Player('test', 'blue');
   expect(player.toString()).toBe('test (blue)');
-})
+});
 
 test('setName', () => {
   const player = new Player('test', 'blue');
   player.name = 'someOtherName';
   expect(player.name).toBe('someOtherName');
-})
+});
 
 test('test no name', () => {
   const t = () => new Player();
   expect(t).toThrow(new Error('Invalid player name: "undefined"'));
-})
+});
 
 test('change name error throw', () => {
   const player = new Player('test');
