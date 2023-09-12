4946b083732cd5c2fa27cd232e4e5de01a774c33
lelonS <35070147+lelonS@users.noreply.github.com>
2023-09-12 16:29:26 +0200

    Create cypress tests for winning and playing again

--- tests-cypress/step_definitions/gameplay.js
+++ tests-cypress/step_definitions/gameplay.js
@@ -1,27 +1,34 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given('I am on the main page', () => {
-  // TODO: implement step
+  cy.visit('/');
 });
 
 Given('I enter valid player names', () => {
-  // TODO: implement step
+  cy.get('input').eq(0).type('Alice');
+  cy.get('input').eq(1).type('Bob');
+  cy.get('button').click();
 });
 
 When('the game is played to a win', () => {
-  // TODO: implement step
+  const moves = [0, 1, 0, 1, 0, 1, 0];
+  moves.forEach(move => {
+    cy.get('.column').eq(move).click();
+  });
 });
 
 When('I click on the {string} button', (text) => {
-  // TODO: implement step
+  cy.get('button').contains(text).click();
 });
 
 Then('the board is reset', () => {
-  // TODO: implement step
+  cy.get('.board .player-1').should('not.exist');
+  cy.get('.board .player-2').should('not.exist');
 });
 
 Then('the game should start', () => {
-  // TODO: implement step
+  // If game started, there should be a message about whose turn it is
+  cy.get('.game-info h3').should('contain', 'turn');
 });
 
 When('The game is played to a draw', () => {
