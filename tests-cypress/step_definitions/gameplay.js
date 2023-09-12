93bbf051fa7b5187856dd34791ec07d72af1d53a
lelonS <35070147+lelonS@users.noreply.github.com>
2023-09-12 11:23:28 +0200

    Create cypress javascript files

---
+++ tests-cypress/step_definitions/gameplay.js
@@ -0,0 +1,49 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given('I am on the main page', () => {
+  // TODO: implement step
+});
+
+Given('I enter valid player names', () => {
+  // TODO: implement step
+});
+
+When('the game is played to a win', () => {
+  // TODO: implement step
+});
+
+When('I click on the {string} button', (text) => {
+  // TODO: implement step
+});
+
+Then('the board is reset', () => {
+  // TODO: implement step
+});
+
+Then('the game should start', () => {
+  // TODO: implement step
+});
+
+When('The game is played to a draw', () => {
+  // TODO: implement step
+});
+
+Then('I should be able to enter player names', () => {
+  // TODO: implement step
+});
+
+Given('no names have been entered', () => {
+  // TODO: implement step
+});
+
+When('I click on a column', () => {
+  // TODO: implement step
+});
+
+Then('no move is made', () => {
+  // TODO: implement step
+});
+
+Then('the {string} div should have the {string} class', (divClass, otherDivClass) => {
+  // TODO: implement step
+});
---
+++ tests-cypress/step_definitions/player-names.js
@@ -0,0 +1,25 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+When('I click the {string} button', (text) => {
+  // TODO: implement step
+});
+
+Then('the game should not start', () => {
+  // TODO: implement step
+});
+
+When('I enter {string} in the {string} field', (text, b) => {
+  // TODO: implement step
+});
+
+Then('the player name input fields should be empty', () => {
+  // TODO: implement step
+});
+
+Then('{string} should be in the {string} field', (text, b) => {
+  // TODO: implement step
+});
+
+Then('the {string} field should be empty', (a) => {
+  // TODO: implement step
+});
