c6161bef855bd8f73e1e54fde376ea3b577ff357
Sherif <sherifmagdy@gmail.com>
2023-09-12 20:00:04 +0200

    FEAT: added step definitions implementations for player-name cypress tests

--- tests-cypress/step_definitions/player-names.js
+++ tests-cypress/step_definitions/player-names.js
@@ -1,25 +1,34 @@
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 When('I click the {string} button', (text) => {
-  // TODO: implement step
+  switch (text) {
+    case 'Start Game': {
+      cy.get('button').click();
+      break;
+    }
+    default: {
+      cy.get(text).click();
+    }
+  }
 });
 
 Then('the game should not start', () => {
-  // TODO: implement step
+  cy.get('.game-info h3').should('not.contain', 'turn');
 });
 
 When('I enter {string} in the {string} field', (text, b) => {
-  // TODO: implement step
+  cy.get(`input[placeholder = '${b}']`).type(text);
 });
 
 Then('the player name input fields should be empty', () => {
-  // TODO: implement step
+  cy.get('input').eq(0).should('have.value', '');
+  cy.get('input').eq(1).should('have.value', '');
 });
 
 Then('{string} should be in the {string} field', (text, b) => {
-  // TODO: implement step
+  cy.get(`input[ placeholder = '${b}']`).should('have.value', text);
 });
 
 Then('the {string} field should be empty', (a) => {
-  // TODO: implement step
+  cy.get(`input[ placeholder = '${a}']`).should('have.value', '');
 });
