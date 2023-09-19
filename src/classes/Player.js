cc98fcef6823fa325b51917dd88904ed6fabd972
lelonS <35070147+lelonS@users.noreply.github.com>
2023-09-19 15:23:10 +0200

    Move comment

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -2,13 +2,12 @@ class Player {
 
   static get nameMinLength() { return 1; }
   static get nameMaxLength() { return 10; }
-  static get nameRegex() { return /^[a-zA-Z0-9]+$/; }
+  static get nameRegex() { return /^[a-zA-Z0-9]+$/; }  // Only allow letters and numbers
 
   static isValidName(name) {
     if (typeof name !== 'string') { return false; }
 
     if (name.length < Player.nameMinLength || name.length > Player.nameMaxLength) { return false; }
-    // Only allow letters in name and numbers
     if (!name.match(Player.nameRegex)) { return false; }
     return true;
   }
