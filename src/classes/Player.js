14c8a65ef224b4ecae0ac80c26916a98ec523987
lelonS <35070147+lelonS@users.noreply.github.com>
2023-09-19 15:07:08 +0200

    Make new valid name rules

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,9 +1,15 @@
 class Player {
 
+  static get nameMinLength() { return 1; }
+  static get nameMaxLength() { return 10; }
+  static get nameRegex() { return /^[a-zA-Z0-9]+$/; }
+
   static isValidName(name) {
     if (typeof name !== 'string') { return false; }
-    // Only allow letters in name.
-    if (!name.match(/^[a-zA-Z]+$/)) { return false; }
+
+    if (name.length < Player.nameMinLength || name.length > Player.nameMaxLength) { return false; }
+    // Only allow letters in name and numbers
+    if (!name.match(Player.nameRegex)) { return false; }
     return true;
   }
 
