2859c718d908447b00d7350afd0ec482c1b4cbec
lelonS <35070147+lelonS@users.noreply.github.com>
2023-09-19 09:14:14 +0200

    Remove extra code

--- src/classes/Bot.js
+++ src/classes/Bot.js
@@ -1,13 +1,6 @@
 class Bot extends Player {
-  constructor() {
-    super('bot', Math.random());
-  }
 
-  /* Get the next move for bot
-   * @param {Board} board
-   * @returns number
-  */
-  getMove(_board) {
-    return 0;
+  getMove(board) {
+
   }
 }
--- src/classes/SmartBot.js
+++ src/classes/SmartBot.js
@@ -1,43 +1,6 @@
 class SmartBot extends Bot {
-  constructor() {
-    super();
-  }
 
-  /*
-   * @Override
-   * @param {Board} board
-   * @returns {number}
-  */
   getMove(board) {
-    let validCols = [];
-    for (let i = 0; i < board.colCount; i++) {
-      if (board.board[i].length < board.rowCount) {
-        validCols.push(i);
-      }
-    }
-    if (validCols.length === 0) {
-      throw 'No valid move to make!';
-    }
-    // since it is our turn we can consider our number is this
-    const playerNumber = board.turn;
-    let highestCount = 0;
-    let optimalCol = validCols[0];
-    for (let i = 0; i < validCols.length; i++) {
-      const row = board.board[validCols[i]];
-      let consecutiveCount = 0;
 
-      for (let i = row.length - 0; i >= 0; i++) {
-        if (row[i] != playerNumber) {
-          break;
-        }
-        consecutiveCount += 1;
-      }
-
-      if (consecutiveCount > highestCount) {
-        highestCount = consecutiveCount;
-        optimalCol = validCols[i];
-      }
-    }
-    return optimalCol;
   }
 }
