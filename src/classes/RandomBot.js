b7a18148dc90ea7db2e8b2080a2c22ea7cc2e285
lelonS <35070147+lelonS@users.noreply.github.com>
2023-09-20 22:21:35 +0200

    Fix tab spaces

--- src/classes/RandomBot.js
+++ src/classes/RandomBot.js
@@ -1,17 +1,17 @@
 class RandomBot extends Bot {
 
   getMove(board) {
-      // empty cols where we can make a move
-      const validCols = [];
-      for (let i = 0; i < board.colCount; i++) {
-          if (board.board[i].length < board.rowCount) {
-              validCols.push(i);
-          }
+    // non-full cols where we can make a move
+    const validCols = [];
+    for (let i = 0; i < board.colCount; i++) {
+      if (board.board[i].length < board.rowCount) {
+        validCols.push(i);
       }
-      if (validCols.length === 0) {
-          throw new Error('no valid move');
-      }
-      // select a random col
-      return validCols[Math.floor(Math.random() * (validCols.length))];
+    }
+    if (validCols.length === 0) {
+      throw new Error('no valid move');
+    }
+    // select a random col
+    return validCols[Math.floor(Math.random() * (validCols.length))];
   }
 }
