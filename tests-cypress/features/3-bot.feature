6deaa0e9e192eb40597383c1df1a769f8a93990e
babblin <nibla007@hotmail.com>
2023-09-26 16:02:15 +0200

    Make bot cypress test
    Co-Authored-By: lelonS <35070147+lelonS@users.noreply.github.com>

--- tests-cypress/features/3-bot.feature
+++ tests-cypress/features/3-bot.feature
@@ -3,13 +3,13 @@ Feature: Bot
         Given I am on the main page
         When I pick "random-bot" in both dropdowns
         And I enter valid player names
-        Then the game starts
+        Then the game should start
         And Bot and Bot make moves on their turn until the game ends
 
     Scenario: Bot vs Human
         Given I am on the main page
         When I pick "random-bot" in first dropdown
         And I enter valid player names
-        Then The game starts
-        And a move has been made by "player-1"
-        And I can make a move
+        Then the game should start
+        And a move has been made by ".player-1"
+        And I can make a move as ".player-2"
--- tests-cypress/step_definitions/3-bot.js
+++ tests-cypress/step_definitions/3-bot.js
@@ -5,26 +5,19 @@ When('I pick {string} in both dropdowns', (playerType) => {
   cy.get('.game-info select').eq(1).select(playerType);
 });
 
-Then('the game starts', () => {
-  // TODO: implement step
-});
-
 Then('Bot and Bot make moves on their turn until the game ends', () => {
-  // TODO: implement step
+  cy.get('.game-result', { timeout: 60000 }).should('exist');
 });
 
 When('I pick {string} in first dropdown', (playerType) => {
-  // TODO: implement step
-});
-
-Then('The game starts', () => {
-  // TODO: implement step
+  cy.get('.game-info select').eq(0).select(playerType);
 });
 
 Then('a move has been made by {string}', (playerClass) => {
-  // TODO: implement step
+  cy.get('.board ' + playerClass).should('exist');
 });
 
-Then('I can make a move', () => {
-  // TODO: implement step
+Then('I can make a move as {string}', (playerClass) => {
+  cy.get('.board .column').eq(0).click();
+  cy.get('.board ' + playerClass).should('exist');
 });
