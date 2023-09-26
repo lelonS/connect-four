63dea421d0bce5c03b953a7a2e1d5cd7e128eadc
babblin <nibla007@hotmail.com>
2023-09-26 15:38:07 +0200

    Update 3-bot.js

--- tests-cypress/step_definitions/3-bot.js
+++ tests-cypress/step_definitions/3-bot.js
@@ -1,8 +1,8 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-When('I pick {string} in both dropdowns', (a) => {
-  cy.get('.game-info select').eq(0).select(a);
-  cy.get('.game-info select').eq(1).select(a);
+When('I pick {string} in both dropdowns', (playerType) => {
+  cy.get('.game-info select').eq(0).select(playerType);
+  cy.get('.game-info select').eq(1).select(playerType);
 });
 
 Then('the game starts', () => {
@@ -13,7 +13,7 @@ Then('Bot and Bot make moves on their turn until the game ends', () => {
   // TODO: implement step
 });
 
-When('I pick {string} in first dropdown', (a) => {
+When('I pick {string} in first dropdown', (playerType) => {
   // TODO: implement step
 });
 
@@ -21,7 +21,7 @@ Then('The game starts', () => {
   // TODO: implement step
 });
 
-Then('a move has been made by {string}', (a) => {
+Then('a move has been made by {string}', (playerClass) => {
   // TODO: implement step
 });
 
