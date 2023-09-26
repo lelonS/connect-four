1547f3108eae593a6b8eb3b35765a450ce1f074e
lelonS <35070147+lelonS@users.noreply.github.com>
2023-09-26 10:29:56 +0200

    Create SmartBot.test.js

---
+++ tests/SmartBot.test.js
@@ -0,0 +1,29 @@
+require('./load-all-classes.js');
+const BoardPositions = require('./BoardPositions.js');
+
+test('Initial SmartBot variables are correct', () => {
+  const bot = new SmartBot('test', 1);
+  expect(bot).toBeInstanceOf(Player);
+  expect(bot.name).toBe('test');
+  expect(bot.plrNumber).toBe(1);
+});
+
+test('getMove returns a valid a move', () => {
+  const bot = new SmartBot('test', 1);
+  const board = new Board();
+  let move = bot.getMove(board);
+  expect(board.isValidMove(move)).toBe(true);
+
+  // Check that getMove returns the only valid move
+  board.board = BoardPositions.lastMoveCol6Win.board;
+  move = bot.getMove(board);
+  expect(board.isValidMove(move)).toBe(true);
+});
+
+test('getMove throws error if no valid moves', () => {
+  const bot = new SmartBot('test', 1);
+  const board = new Board();
+  board.board = BoardPositions.fullDraw.board;
+  const t = () => bot.getMove(board);
+  expect(t).toThrow(Error);
+});
