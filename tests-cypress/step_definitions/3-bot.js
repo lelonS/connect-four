21fd54d74e71f4823feb51f630412f0e8c48f4b8
babblin <nibla007@hotmail.com>
2023-09-26 15:12:46 +0200

    Generate step definitions

---
+++ tests-cypress/step_definitions/3-bot.js
@@ -0,0 +1,33 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+When('I pick {string} in both dropdowns', (a) => {
+  // TODO: implement step
+});
+
+When('I enter valid player names', () => {
+  // TODO: implement step
+});
+
+Then('the game starts', () => {
+  // TODO: implement step
+});
+
+Then('Bot and Bot make moves on their turn until the game ends', () => {
+  // TODO: implement step
+});
+
+When('I pick {string} in first dropdown', (a) => {
+  // TODO: implement step
+});
+
+Then('The game starts', () => {
+  // TODO: implement step
+});
+
+Then('a move has been made by {string}', (a) => {
+  // TODO: implement step
+});
+
+Then('I can make a move', () => {
+  // TODO: implement step
+});
---
+++ tests-cypress/step_definitions/4-reset-button.js
@@ -0,0 +1,9 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+When('the game is in progress', () => {
+  // TODO: implement step
+});
+
+When('I wait for {string} miliseconds', (a) => {
+  // TODO: implement step
+});
