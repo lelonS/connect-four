e04557b428fe0a40eaa8230206c052695bf805cb
lelonS <35070147+lelonS@users.noreply.github.com>
2023-10-02 16:10:23 +0200

    Create Elements.js to create html elements

---
+++ src/classes/Elements.js
@@ -0,0 +1,129 @@
+class Elements {
+
+  static gameContainerElement() {
+    const gameContainer = document.createElement('div');
+    gameContainer.classList.add('game-container');
+
+    const board = document.createElement('div');
+    board.classList.add('board');
+
+    const gameSidebar = document.createElement('div');
+    gameSidebar.classList.add('game-sidebar');
+
+    const gameTitle = document.createElement('h2');
+    gameTitle.textContent = 'Connect 4';
+
+    const gameInfo = document.createElement('div');
+    gameInfo.classList.add('game-info');
+
+    gameSidebar.appendChild(gameTitle);
+    gameSidebar.appendChild(gameInfo);
+    gameContainer.appendChild(board);
+    gameContainer.appendChild(gameSidebar);
+    return gameContainer;
+  }
+
+  static nameInputElement(placeholder) {
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = placeholder;
+    input.maxLength = Player.nameMaxLength;
+    input.minLength = Player.nameMinLength;
+    input.pattern = Player.nameRegex.source;
+
+    return input;
+  }
+
+  static playerTypeDropdownElement() {
+    const playerTypes = Player.PlayerTypes;
+    const dropdown = document.createElement('select');
+    dropdown.innerHTML = /*html*/`
+        <option value="${playerTypes.Human}">Human</option>
+        <option value="${playerTypes.RandomBot}">Random Bot</option>
+        <option value="${playerTypes.SmartBot}">Smart Bot</option>
+        `;
+    return dropdown;
+  }
+
+  static boardCellElement(board, col, row) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+
+    const cellValue = board.getCell(col, row);
+    if (cellValue !== null) {
+      cell.classList.add(`player-${cellValue + 1}`);
+    }
+    return cell;
+  }
+
+  static boardColumnElement(board, col) {
+    const column = document.createElement('div');
+    column.classList.add('column');
+    for (let row = 0; row < board.rowCount; row++) {
+      const cellElement = Elements.boardCellElement(board, col, row);
+      column.appendChild(cellElement);
+    }
+    return column;
+  }
+
+  static newGameButton(game) {
+    const newGameButton = document.createElement('button');
+    newGameButton.textContent = 'New Game';
+    newGameButton.addEventListener('click', () => {
+      game.reset();
+    });
+    return newGameButton;
+  }
+
+  static playAgainButton(game) {
+    const playAgainButton = document.createElement('button');
+    playAgainButton.textContent = 'Play Again';
+    playAgainButton.addEventListener('click', () => {
+      game.reset(false);
+    });
+    return playAgainButton;
+  }
+
+  static resetButton(game) {
+    const resetButton = Elements.newGameButton(game);
+    resetButton.textContent = 'Reset Game';
+    resetButton.classList.add('abort-button');
+    return resetButton;
+  }
+
+  static drawResultHtml() {
+    return /*html*/`
+    <h3 class="game-result">It's a draw!</h3>`;
+  }
+
+  static winResultHtml(winner, winnerIndex) {
+    return /*html*/`
+    <h3 class="game-result">${winner.name} won!</h3>
+    <div class="cell player-${winnerIndex + 1}"></div>`
+  }
+
+  static resultHtml(game) {
+    if (game.board.gameState === Board.GameStates.Draw) {
+      return Elements.drawResultHtml();
+    } else if (game.board.gameState === Board.GameStates.Win) {
+      const winnerIndex = game.board.winner;
+      const winner = game.players[winnerIndex];
+      return Elements.winResultHtml(winner, winnerIndex);
+    }
+  }
+
+  static turnHtml(player, playerIndex) {
+    return /*html*/`
+    <h3>${player.name}'s turn</h3>
+    <!-- Draws a circle with the player's color thats a bit smaller than the cells -->
+    <div class="cell player-${playerIndex + 1}" style="width: calc(var(--board-width) / 7 * 0.6);"></div>`;
+  }
+
+  static playerNameInfoHtml() {
+    return /*html*/`
+    <h3>Enter player names</h3>
+    <p>Only letters and numbers</p>
+    `;
+  }
+
+}
--- src/classes/Game.js
+++ src/classes/Game.js
@@ -9,25 +9,7 @@ class Game {
 
   #createElements() {
     document.body.innerHTML = '';
-    const gameContainer = document.createElement('div');
-    gameContainer.classList.add('game-container');
-
-    const board = document.createElement('div');
-    board.classList.add('board');
-
-    const gameSidebar = document.createElement('div');
-    gameSidebar.classList.add('game-sidebar');
-
-    const gameTitle = document.createElement('h2');
-    gameTitle.textContent = 'Connect 4';
-
-    const gameInfo = document.createElement('div');
-    gameInfo.classList.add('game-info');
-
-    gameSidebar.appendChild(gameTitle);
-    gameSidebar.appendChild(gameInfo);
-    gameContainer.appendChild(board);
-    gameContainer.appendChild(gameSidebar);
+    const gameContainer = Elements.gameContainerElement();
     document.body.appendChild(gameContainer);
   }
 
@@ -49,48 +31,28 @@ class Game {
     // Get .game-info element
     const gameInfo = document.querySelector('.game-info');
     // Clear .game-info
-    gameInfo.innerHTML = /*html*/`
-      <h3>Enter player names</h3>
-      <p>Only letters and numbers</p>
-      `;
+    gameInfo.innerHTML = Elements.playerNameInfoHtml();
 
     // Create input elements
     const nameInputElements = [];
-    for (let i = 0; i < this.playerCount; i++) {
-      const input = document.createElement('input');
-      input.type = 'text';
-      input.placeholder = 'Player' + (i + 1);
-      input.maxLength = Player.nameMaxLength;
-      input.minLength = Player.nameMinLength;
-      input.pattern = Player.nameRegex.source;
-
-      input.classList.add(`player-${i + 1}-border`);
-      nameInputElements.push(input);
-    }
-
-    // Create dropdown elements
-    const playerTypes = Player.PlayerTypes;
     const dropdownElements = [];
     for (let i = 0; i < this.playerCount; i++) {
-      const dropdown = document.createElement('select');
-      dropdown.innerHTML = /*html*/`
-        <option value="${playerTypes.Human}">Human</option>
-        <option value="${playerTypes.RandomBot}">Random Bot</option>
-        <option value="${playerTypes.SmartBot}">Smart Bot</option>
-        `;
+      const input = Elements.nameInputElement(`Player${i + 1}`);
+      input.classList.add(`player-${i + 1}-border`);
+      nameInputElements.push(input);
+
+      const dropdown = Elements.playerTypeDropdownElement();
       dropdown.classList.add(`player-${i + 1}-border`);
       dropdownElements.push(dropdown);
+
+      gameInfo.appendChild(input);
+      gameInfo.appendChild(dropdown);
     }
 
     // Create submit button
     const submitButton = document.createElement('button');
     submitButton.textContent = 'Start';
 
-    // Add elements to .game-info
-    for (let i = 0; i < this.playerCount; i++) {
-      gameInfo.appendChild(nameInputElements[i]);
-      gameInfo.appendChild(dropdownElements[i]);
-    }
     gameInfo.appendChild(submitButton);
 
     // Add event listener to submit button
@@ -135,26 +97,10 @@ class Game {
     const boardElement = document.querySelector('.board');
     // Clear board
     boardElement.innerHTML = '';
-
     //Create columns
     for (let col = 0; col < this.board.colCount; col++) {
-      const colElement = document.createElement('div');
-      colElement.classList.add('column');
+      const colElement = Elements.boardColumnElement(this.board, col);
       boardElement.appendChild(colElement);
-
-      // Create cells
-      for (let row = 0; row < this.board.rowCount; row++) {
-        const cellElement = document.createElement('div');
-        cellElement.classList.add('cell');
-        colElement.appendChild(cellElement);
-
-        // Get cell value
-        const cellValue = this.board.getCell(col, row);
-        if (cellValue !== null) {
-          const player = this.players[cellValue];
-          cellElement.classList.add(`player-${player.plrNumber}`);
-        }
-      }
     }
   }
 
@@ -164,57 +110,30 @@ class Game {
     const boardElement = document.querySelector('.board');
     const columnElement = boardElement.querySelectorAll('.column')[col];
     const cell = columnElement.querySelectorAll('.cell')[row];
-    cell.classList.add(`player-${player.plrNumber}`);
+    cell.classList.add(`player-${turn + 1}`);
   }
 
   renderResults() {
     const gameInfo = document.querySelector('.game-info');
     // Write result to sidebar
-    if (this.board.gameState === Board.GameStates.Draw) {
-      gameInfo.innerHTML = /*html*/`
-        <h3 class="game-result">It's a draw!</h3>`
-    } else if (this.board.gameState === Board.GameStates.Win) {
-      const winnerIndex = this.board.winner;
-      const winner = this.players[winnerIndex];
-      gameInfo.innerHTML = /*html*/`
-        <h3 class="game-result">${winner.name} won!</h3>
-        <div class="cell player-${winner.plrNumber}"></div> `
-    }
+    gameInfo.innerHTML = Elements.resultHtml(this);
 
     // Play again buttons
-    const newGameButton = document.createElement('button');
-    newGameButton.textContent = 'New Game';
+    const newGameButton = Elements.newGameButton(this);
     gameInfo.appendChild(newGameButton);
-    newGameButton.addEventListener('click', () => {
-      gameInfo.innerHTML = '';
-      this.reset();
-    });
 
-    const playAgainButton = document.createElement('button');
-    playAgainButton.textContent = 'Play Again';
+    const playAgainButton = Elements.playAgainButton(this);
     gameInfo.appendChild(playAgainButton);
-    playAgainButton.addEventListener('click', () => {
-      gameInfo.innerHTML = '';
-      this.reset(false);
-    });
 
   }
 
   renderTurn() {
     const gameInfo = document.querySelector('.game-info');
     const player = this.players[this.board.turn];
-    gameInfo.innerHTML = /*html*/`
-      <h3>${player.name}'s turn</h3>
-      <!-- Draws a circle with the player's color thats a bit smaller than the cells -->
-      <div class="cell player-${player.plrNumber}" style="width: calc(var(--board-width) / 7 * 0.6);"></div>`;
-    let newGameButton = document.createElement('button');
-    newGameButton.innerText = 'Reset Game';
-    newGameButton.classList.add('abort-button');
-
-    newGameButton.addEventListener('click', (e) => {
-      this.reset();
-    });
-    gameInfo.appendChild(newGameButton);
+    gameInfo.innerHTML = Elements.turnHtml(player, this.board.turn);
+    // Add reset button
+    let resetButton = Elements.resetButton(this);
+    gameInfo.appendChild(resetButton);
   }
 
   #playBotMove() {
